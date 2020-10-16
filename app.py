1ea19087ac25ebbfd0860cdacb1d9aacb49a19b9
Justin <duckweiser@gmail.com>
2020-10-15 23:20:06 -0500

    add'l flask work

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ from flask import Flask, jsonify
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
-Base = automap_base()
-Base.prepare(engine, reflect=True)
+base = automap_base()
+base.prepare(engine, reflect=True)
 
 # Save references to each table
 measurement1 = base.classes.measurement
@@ -24,10 +24,10 @@ station1 = base.classes.station
 #################################################
 # STATIC DICTIONARIES
 #################################################
-justice_league_members = [
-    {"superhero": "Aquaman", "real_name": "Arthur Curry"},
-
-]
+#example
+#justice_league_members = [
+#    {"superhero": "Aquaman", "real_name": "Arthur Curry"},
+#]
 
 #################################################
 # Flask Setup
@@ -65,26 +65,64 @@ def justice_league_dict():
 #JSON JSON JSON
 @app.route("/api/v1.0/precipitation")
 def precipitation_page():
-    """Return the precipitation data as json"""
+    """Return a list of precipitation data"""
 
-    return jsonify(precipitation_listo)
+    # Open a communication session with the database
+    session = Session(engine)
+
+    # Query all passengers
+    results = session.query(measurement1).all()
+
+    # close the session to end the communication with the database
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of precip
+    precip = []
+    for result in results:
+        precip_dict = {}
+        precip_dict["date"] = measurement1.date
+        precip_dict["prcp"] = measurement1.prcp
+        precip.append(precip_dict)
+
+    return jsonify(precip)
 
 @app.route("/api/v1.0/stations")
+# Return a JSON list of stations from the dataset.
 def stations_page():
+    """Return a list of all Station names"""
+
+    # Query all stations
+    session = Session(engine)
+    results = session.query(station1.name).all()
+
+    # close the session to end the communication with the database
+    session.close()
+
+    # Convert list of tuples into normal list
+    all_names = list(np.ravel(results))
+    print(all_names)
+    return jsonify(all_names)
+
+@app.route("/api/v1.0/tobs")
+#Query the dates and temperature observations of the most active station for the last year of data.
+#Return a JSON list of temperature observations (TOBS) for the previous year.
+
+def tobs_page():
     """Return the stations data as json"""
 
     return jsonify(stations_listo)
 
-@app.route("/api/v1.0/stations")
-def stations_page():
-    """Return the stations data as json"""
 
-    return jsonify(stations_listo)
+
+#@app.route("/api/v1.0/<start>")
+
+#@app.route("/api/v1.0/<start>/<end>")
 
 
 # Index route
 
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
+
